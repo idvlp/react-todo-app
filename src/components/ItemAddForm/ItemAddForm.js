a48565ea9a48ca96e10f6dd47c7bcb0dc2799e7d
Node-laptop <neverused80@yandex.ru>
2021-03-16 13:52:44 +0300

    Add form input for task label

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -3,19 +3,41 @@ import './ItemAddForm.css'
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        console.log(e);
+        this.setState({label: e.target.value})
+
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAddItem(this.state.label);
+        this.setState({label: ''})
+    }
+
 
     render() {
-        const {onAddItem} = this.props;
-
         return (
-            <div className="item-add-form">
+            <form 
+                className="item-add-form d-flex"
+                onSubmit={this.onSubmit}
+            >
+                <input 
+                    type="text" 
+                    className="form-control flex-fill"
+                    onChange={this.onLabelChange} 
+                    value={this.state.label} 
+                />
                 <button 
-                    className="btn btn-outline-secondary"
-                    onClick={() => onAddItem("text")}
+                    className="btn btn-outline-secondary flex-fill"   
                 >
                 Add item
                 </button>
-            </div>
+            </form>
         )
     }
 }
